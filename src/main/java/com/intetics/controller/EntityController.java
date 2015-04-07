64b540d9bdada33045253564eb0986b92d0da367
Anton Chervyakovsky <a.chervyakovsky@intetics.com>
2015-04-07 12:44:13 +0300

    Task AR009604-84: Create project skeleton - added logging via slf4j

--- src/main/java/com/intetics/controller/EntityController.java
+++ src/main/java/com/intetics/controller/EntityController.java
@@ -1,5 +1,7 @@
 package com.intetics.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,11 +14,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/entity")
 public class EntityController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(EntityController.class);
+
     @RequestMapping(value = "/list", method = RequestMethod.GET)
     public String getList(ModelMap model) {
 
 //        List<EntityName> entityNames = baseService.getAll(EntityName.class);
 //        model.addAttribute("entityNamesList", entityNames);
+        LOGGER.trace("The list of entities has been requested");
         return "entity-list";
+
     }
 }
