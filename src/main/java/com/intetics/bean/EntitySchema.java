ce4b7f8cd09bed83869b366156368b16628f556d
nikolay.kvetko <nikolay.kvetko@gmail.com>
2015-04-13 15:47:54 +0300

    Task AR009604-112: Add validation on Entity operations - annotate EntitySchema with the constraints which will validate

--- src/main/java/com/intetics/bean/EntitySchema.java
+++ src/main/java/com/intetics/bean/EntitySchema.java
@@ -1,17 +1,35 @@
 package com.intetics.bean;
 
+import com.intetics.validation.DuplicateEntity;
+import org.hibernate.validator.constraints.NotEmpty;
+
 import javax.persistence.*;
+import javax.validation.GroupSequence;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Entity
 @Table(name = "entity_schema")
+@GroupSequence({
+        NotEmpty.class,
+        Size.class,
+        EntitySchema.class
+})
 public class EntitySchema {
+
+    @GroupSequence({
+            DuplicateEntity.class
+    })
+    public interface MvcValidationSequence {}
 
     @Id
     @Column(name = "entity_schema_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotEmpty(groups = NotEmpty.class)
+    @Size(min = 2, max = 30, groups = Size.class)
+    @DuplicateEntity(groups = DuplicateEntity.class)
     @Column(name = "name")
     private String name;
 
